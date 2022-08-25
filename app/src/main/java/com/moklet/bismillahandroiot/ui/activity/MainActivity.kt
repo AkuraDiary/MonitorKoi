6d84bb6e45daf01c0b6656b0fbf911227b8ef5e7
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-08-26 00:49:06 +0700

    trying to run it

--- app/src/main/java/com/moklet/bismillahandroiot/ui/activity/MainActivity.kt
+++ app/src/main/java/com/moklet/bismillahandroiot/ui/activity/MainActivity.kt
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.moklet.bismillahandroiot.R
 import com.moklet.bismillahandroiot.databinding.ActivityMainBinding
+import com.moklet.bismillahandroiot.ui.fragments.HistoryFragment
+import com.moklet.bismillahandroiot.ui.fragments.MainFragment
 
 class MainActivity : AppCompatActivity() {
-    private var binding :ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+
+        initFragments()
+    }
+
+    private fun initFragments() {
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.mainViewFragmentContainer, MainFragment())
+            .add(R.id.historyViewFragmentContainer, HistoryFragment())
+            .commit()
     }
 
     override fun onDestroy() {
