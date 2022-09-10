01e3bb587b0a8fbf1ab06e21d9062a7b5994c469
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-09-10 21:02:55 +0700

    added feed model and feed responses

---
+++ app/src/main/java/com/moklet/bismillahandroiot/data/remote/FeedModel.kt
@@ -0,0 +1,12 @@
+package com.moklet.bismillahandroiot.data.remote
+
+data class FeedModel(
+    val created_at : String,
+    val entry_id : Int,
+    val field1 : String,
+    val field2 : String,
+    val field3 : String,
+    val field4 : String,
+    val field5 : String,
+
+)
---
+++ app/src/main/java/com/moklet/bismillahandroiot/data/remote/FeedsResponse.kt
@@ -0,0 +1,8 @@
+package com.moklet.bismillahandroiot.data.remote
+
+import com.moklet.bismillahandroiot.data.model.DataModel
+
+
+data class FeedsResponse(
+    val feeds: List<FeedModel>
+)
